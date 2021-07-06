317ab4762c1c0926820df26a78941239926a4610
huahua0303 <84437813+huahua0303@users.noreply.github.com>
2021-07-06 10:15:33 +0800

    Update CsMain.java

--- src/main/java/com/cn/fanfan/main/CsMain.java
+++ src/main/java/com/cn/fanfan/main/CsMain.java
@@ -6,5 +6,6 @@ public class CsMain {
         System.out.println("第二次提交");
         System.out.println("第三次提交稍后进行分支提交和合并");
         System.out.println("提交之后第一个分支进行了修改，稍后合并分支");
+        System.out.println("恭喜您陈功拉取代码到本地");
     }
 }
