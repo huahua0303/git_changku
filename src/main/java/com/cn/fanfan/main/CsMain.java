2a47f53aaa5adecd1935121178653faddf086b0c
fan <huahua03_03@163.com>
2021-07-06 08:37:20 +0800

    第二次测试提交

--- src/main/java/com/cn/fanfan/main/CsMain.java
+++ src/main/java/com/cn/fanfan/main/CsMain.java
@@ -3,5 +3,6 @@ package com.cn.fanfan.main;
 public class CsMain {
     public static void main(String[] args) {
         System.out.println("我是第一此提交");
+        System.out.println("第二次提交");
     }
 }
