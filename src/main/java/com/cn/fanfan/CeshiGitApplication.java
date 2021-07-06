b5193a2e280e385d45d1cfac79a614687cf9c177
fan <huahua03_03@163.com>
2021-07-06 08:34:34 +0800

    第一次测试提交

---
+++ src/main/java/com/cn/fanfan/CeshiGitApplication.java
@@ -0,0 +1,13 @@
+package com.cn.fanfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeshiGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeshiGitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cn/fanfan/main/CsMain.java
@@ -0,0 +1,7 @@
+package com.cn.fanfan.main;
+
+public class CsMain {
+    public static void main(String[] args) {
+        System.out.println("我是第一此提交");
+    }
+}
